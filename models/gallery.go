13788f1d4f9f5d7098a56f3f9ff1c746eb26ca75
Silas Burger <ybsilas@gmail.com>
2025-04-11 18:28:11 +0200

    images in gallery service
    Signed-off-by: Silas Burger <ybsilas@gmail.com>

--- models/gallery.go
+++ models/gallery.go
@@ -4,6 +4,8 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+	"path/filepath"
+	"strings"
 )
 
 type Gallery struct {
@@ -13,8 +15,19 @@ type Gallery struct {
 	Published bool
 }
 
+type Image struct {
+	Path      string
+	Filename  string
+	GalleryID int
+}
+
 type GalleryService struct {
 	DB *sql.DB
+
+	// ImagesDir is used to tell the GalleryService where to store and locate
+	// images. If not set, the GalleryService will default to using the "images"
+	// directory.
+	ImagesDir string
 }
 
 func (gs *GalleryService) Create(title string, userID int, published bool) (*Gallery, error) {
@@ -95,4 +108,49 @@ func (gs *GalleryService) Delete(id int) error {
 		return fmt.Errorf("delete gallery: %w", err)
 	}
 	return nil
+}
+
+func (service GalleryService) Images(galleryID int) ([]Image, error) {
+	globPattern := filepath.Join(service.galleryDir(galleryID), "*")
+	allFiles, err := filepath.Glob(globPattern)
+	if err != nil {
+		return nil, fmt.Errorf("receiving gallery images: %w", err)
+	}
+
+	var images []Image
+
+	for _, file := range allFiles {
+		if hasExtension(file, service.extensions()) {
+			images = append(images, Image{
+				Path:      file,
+				Filename:  filepath.Base(file),
+				GalleryID: galleryID,
+			})
+		}
+	}
+
+	return images, nil
+}
+
+func (service *GalleryService) extensions() []string {
+	return []string{".png", ".jpg", ".jpeg", ".gif"}
+}
+
+func (service GalleryService) galleryDir(id int) string {
+	imagesDir := service.ImagesDir
+	if imagesDir == "" {
+		imagesDir = "images"
+	}
+	return filepath.Join(imagesDir, fmt.Sprintf("gallery-%d", id))
+}
+
+func hasExtension(file string, extensions []string) bool {
+	for _, ext := range extensions {
+		file = strings.ToLower(file)
+		ext = strings.ToLower(ext)
+		if filepath.Ext(file) == ext {
+			return true
+		}
+	}
+	return false
 }
